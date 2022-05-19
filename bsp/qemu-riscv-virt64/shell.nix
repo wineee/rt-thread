a29f0646dc0c190bbddbeee9ad5f1b75ae9fdfad
rewine <1758075541@qq.com>
2022-05-20 02:03:53 +0800

    add nix shell build

---
+++ bsp/qemu-riscv-virt64/shell.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  my-python = pkgs.python3;
+  python-with-my-packages = my-python.withPackages (p: with p; [
+    tkinter
+  ]);
+in
+with pkgs; mkShell {
+  buildInputs = [
+    scons
+    gcc
+    ncurses5
+    python-with-my-packages
+  ];
+
+  shellHook = ''
+    RTT_ROOT=/data/rv/rt-thread
+    BSP_ROOT=/data/rv/rt-thread/bsp
+    RTT_CC=gcc
+    EXEC_PATH=/opt/riscv/bin
+    PYTHONPATH=${python-with-my-packages}/${python-with-my-packages.sitePackages}
+    # maybe set more env-vars
+  '';
+}
